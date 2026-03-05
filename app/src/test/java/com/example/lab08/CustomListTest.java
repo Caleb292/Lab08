e256ebb8db5c484bf36933bdbf9c747eb5e035a5
Caleb Tablan <caleb.m.tablan@gmail.com>
2026-03-04 17:55:13 -0700

    Added hasCity() in CustomListTest.java. Should fail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,21 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    void testHasCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "Alberta");
+        City calgary = new City("Calgary", "Alberta");
+
+        list.addCity(edmonton);
+
+        // Calgary is not in the list → should return false
+        assertFalse(list.hasCity(calgary));
+
+        // Edmonton is in the list → should return true
+        assertTrue(list.hasCity(edmonton));
+    }
 }
