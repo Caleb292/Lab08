37024d35e283f0c43408dbcae8f6759bf47e60d6
Caleb Tablan <caleb.m.tablan@gmail.com>
2026-03-04 18:30:41 -0700

    Failing test for countCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,18 +13,48 @@ public class CustomListTest {
 
         list.addCity(edmonton);
 
-        // Calgary is not in the list → should return false
+        // Calgary is not in the list. should return false
         assertFalse(list.hasCity(calgary));
 
-        // Edmonton is in the list → should return true
+        // Edmonton is in the list. should return true
         assertTrue(list.hasCity(edmonton));
+    }
 
-        // Deleting a city not in list → should throw error
+    @Test
+    void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "Alberta");
+        City calgary = new City("Calgary", "Alberta");
+
+        list.addCity(edmonton);
+
+        // Deleting a city not in list. should throw
         assertThrows(IllegalArgumentException.class, () -> list.deleteCity(calgary));
 
-        // Deleting a city in list → should succeed
+        // Deleting a city in list. should succeed
         list.deleteCity(edmonton);
         assertFalse(list.hasCity(edmonton));
     }
+
+    @Test
+    void testCountCities() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "Alberta");
+        City calgary = new City("Calgary", "Alberta");
+
+        // Initial list is empty. count = 0
+        assertEquals(0, list.countCities());
+
+        // Add Edmonton. count = 1
+        list.addCity(edmonton);
+        assertEquals(1, list.countCities());
+
+        // Add Calgary. count = 2
+        list.addCity(calgary);
+        assertEquals(2, list.countCities());
+
+        // Delete Edmonton. count = 1
+        list.deleteCity(edmonton);
+        assertEquals(1, list.countCities());
     }
 }
